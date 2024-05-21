e8c1deef1534354a5ff48657660761d3ac7d873d
mongdori00@naver.com <mongdori00@naver.com>
2024-05-21 19:29:12 +0900

    develop start

---
+++ src/main/java/com/TimeVenture/common/controller/FrontController.java
@@ -0,0 +1,75 @@
+package com.TimeVenture.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontController {
+
+    @GetMapping("/project/kanban")
+    public String kanbanBoardList() {
+        return "projectKanbanBoard/kanbanBoardList";
+    }
+
+    @GetMapping("/main")
+    public String Main() {
+        return "common/main";
+    }
+
+    @GetMapping("/project/attachments")
+    public String Attachments() {
+        return "projectFile/attachment";
+    }
+
+    @GetMapping("/project/list")
+    public String ProjectList() {
+        return "projectList/projectList";
+    }
+
+    @GetMapping("/home")
+    public String Home() {
+        return "projectHome/home";
+    }
+
+    @GetMapping("/project")
+    public String Project() {
+        return "common/project";
+    }
+
+    @GetMapping("/project/start")
+    public String ProjectStart() {
+        return "projectStart/outline";
+    }
+
+    @GetMapping("/project/home")
+    public String ProjectHome() {
+        return "projectHome/home";
+    }
+
+    @GetMapping("/project/dash")
+    public String ProjectDash() {
+        return "projectDashBoard/dashboard";
+    }
+
+    @GetMapping("/project/chatting")
+    public String ProjectChatting() {
+        return "projectChatting/chatting";
+    }
+
+    @GetMapping("/project/calendar")
+    public String ProjectCalendar() {
+        return "projectCalendar/caldenar";
+    }
+
+    @GetMapping("/mypage")
+    public String mypage() {
+        return "member/myPage";
+
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "member/login";
+    }
+
+}
+
